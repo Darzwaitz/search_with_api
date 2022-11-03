a7d68334017618be9bb7acbd0c4340517561ad0c
darzwaitz <darzwaitz@yahoo.com>
2022-11-03 17:50:17 +0000

    Implementing search results - Part 1

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,7 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -31,6 +32,8 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     // get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -40,6 +43,7 @@ const controlSearchResults = async function () {
 
     // render search resultz
     console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
