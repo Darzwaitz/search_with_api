75b752a50167714dd9c24b2b442ca238963960ce
darzwaitz <darzwaitz@yahoo.com>
2022-11-15 20:58:18 +0000

    implementing pagination - logic

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -3,8 +3,28 @@ import icons from '../../img/icons.svg';
 
 class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
+
   _generateMarkup() {
-    // const numPages = ;
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+
+    // Page 1, there are other pagez
+    if (this._data.page === 1 && numPages > 1) {
+      return `Page 1, others`;
+    }
+
+    // Last page
+    if (this._data.page === numPages && numPages > 1) {
+      return 'last page';
+    }
+
+    // Other page
+    if (this._data.page < numPages) {
+      return 'other';
+    }
+    // Page 1, there are NO other pagez
+    return 'only 1 page';
   }
 }
 
