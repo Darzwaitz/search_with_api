a7c3b94b737e7f9ea4b463ecb903449cafab0036
darzwaitz <darzwaitz@yahoo.com>
2022-11-30 13:02:37 +0000

    ignored config file - set up API call - working

--- src/js/controller.js
+++ src/js/controller.js
@@ -107,6 +107,8 @@ const controlAddRecipe = async function (newRecipe) {
   try {
     // Upload the new recipe
     await model.uploadRecipe(newRecipe);
+
+    console.log(model.state.recipe);
   } catch (err) {
     console.error('👢', err);
     addRecipeView.renderError(err.message);
